f512a6cd0ba7a216405b6ef7856bf232ed4ed6ca
muskan <muskantuteja714@gmail.com>
2026-06-09 23:27:55 +0530

    start project

---
+++ app/blog/[id]/page.js
@@ -0,0 +1,6 @@
+const SingleArticlePage = () => {
+
+  // data fatch
+  return <div>SingleArticlePage</div>;
+};
+export default SingleArticlePage;
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,15 +1,31 @@
-export default function BlogPage(){
+export default function BlogPage() {
   const posts = [
     {
-      id:1,
-      title: 'post1',
-      body: 'post1 body'
+      id: 1,
+      title: "post 1",
+      body: "post 1 body",
     },
     {
-      id:1,
-      title: 'post1',
-      body: 'post1 body'
+      id: 2,
+      title: "post 2",
+      body: "post 2 body",
     },
-  ]
-  return<h1>blog post</h1>
+  ];
+  return (
+    <div>
+      <h1>blog post</h1>
+      <div>
+        {posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <a href={`/blog/${post.id}`}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
