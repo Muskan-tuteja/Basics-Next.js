a0822ca256f23d5bc4267e6b89b8b12eb3f97fc1
muskan <muskantuteja714@gmail.com>
2026-06-10 00:16:38 +0530

    fetch data

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,18 +1,24 @@
 import Link from "next/link";
 
-export default function BlogPage() {
-  const posts = [
-    {
-      id: 1,
-      title: "post 1",
-      body: "post 1 body",
-    },
-    {
-      id: 2,
-      title: "post 2",
-      body: "post 2 body",
-    },
-  ];
+export default async function BlogPage() {
+
+  //db call
+
+   const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const posts = await data.json()
+
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: "post 1",
+  //     body: "post 1 body",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "post 2",
+  //     body: "post 2 body",
+  //   },
+  // ];
   return (
     <div>
       
