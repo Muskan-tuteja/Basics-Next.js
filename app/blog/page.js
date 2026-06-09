74673dc5cc59ca20af3b30e6603012bc3571bc5b
muskan <muskantuteja714@gmail.com>
2026-06-09 22:40:02 +0530

    start project

---
+++ app/blog/page.js
@@ -0,0 +1,15 @@
+export default function BlogPage(){
+  const posts = [
+    {
+      id:1,
+      title: 'post1',
+      body: 'post1 body'
+    },
+    {
+      id:1,
+      title: 'post1',
+      body: 'post1 body'
+    },
+  ]
+  return<h1>blog post</h1>
+}
